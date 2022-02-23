878981bdc5d8c07ef6c01d9e2a35aa4964a6e38e
RafaVlack <rafavlack@gmail.com>
2022-02-23 18:56:00 -0500

    Initial commit

---
+++ src/main/java/com/javadev/gateway_sentinel/GatewaySentinelApplication.java
@@ -0,0 +1,13 @@
+package com.javadev.gateway_sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaySentinelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javadev/gateway_sentinel/GatewaySentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev.gateway_sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
